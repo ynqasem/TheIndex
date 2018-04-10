f64f0b4d22b68d137dccaf934ae798951f4fc3bf
Octowl <alsaff1987@gmail.com>
2018-04-10 15:00:09 +0300

    change author names to links

--- src/BookRow.js
+++ src/BookRow.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function BookRow(props) {
   const book = props.book
+  const authors = book.authors
+    .map(
+      author => <Link key={author.name} to={`/authors/${author.id}`}>{author.name}</Link>
+    )
+    .reduce((prev, curr) => [prev, ', ', curr]);
   return (
     <tr>
       <td>{book.title}</td>
-      <td>{book.authors && book.authors.map(author => author.name).join(", ")}</td>
+      <td>{authors}</td>
       <td>
         <button className="btn" style={{backgroundColor: book.color}}/>
       </td>
