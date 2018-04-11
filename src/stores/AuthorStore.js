8900948dcc1d2c24b035473155b0c3ec082785b5
Octowl <alsaff1987@gmail.com>
2018-04-11 14:35:05 +0300

    Implement AuthorStore

---
+++ src/stores/AuthorStore.js
@@ -0,0 +1,27 @@
+import {extendObservable} from 'mobx';
+import axios from 'axios';
+
+import bookStore from './BookStore';
+
+class AuthorStore {
+  constructor() {
+    extendObservable(this, {
+      authors: []
+    });
+  }
+
+  fetchAllAuthors() {
+    return axios.get('https://the-index-api.herokuapp.com/api/authors/')
+            .then(res => res.data)
+            .then(authors => {
+              this.authors = authors
+            })
+            .catch(err => console.error(err));
+  }
+
+  getAuthorById(id) {
+    return this.authors.find(author => author.id == id);
+  }
+}
+
+export default new AuthorStore();
