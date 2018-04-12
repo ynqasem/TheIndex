630fe313ce17056efd79e6011d55ab9ce14b6196
Octowl <alsaff1987@gmail.com>
2018-04-12 14:53:19 +0300

    Add loading state to BookList

--- src/BookList.js
+++ src/BookList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
+import Loading from './Loading';
 import SearchBar from './SearchBar';
 import BookTable from './BookTable';
 
@@ -10,31 +11,34 @@ class BookList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      loading: true
     }
   }
 
   componentDidMount() {
     axios.get("https://the-index-api.herokuapp.com/api/books/")
       .then(res => res.data)
-      .then(books => this.setState({books}))
+      .then(books => this.setState({
+        books,
+        loading: false
+      }))
       .catch(err => console.error(err));
   }
 
   render() {
     const bookColor = this.props.match.params.bookColor;
     const books = this.state.books;
-    return (
-      <div className="books">
-        <h3>Books</h3>
-        <SearchBar store={{}}/>
-        {bookColor &&
-          <Link to="/books">
-            <button className="btn">All Books</button>
-          </Link>}
-        <BookTable books={bookColor ? books.filter(book => book.color === bookColor) : books} />
-      </div>
-    );
+    return this.state.loading ? <Loading /> :
+            <div className="books">
+              <h3>Books</h3>
+              <SearchBar store={{}}/>
+              {bookColor &&
+                <Link to="/books">
+                <button className="btn">All Books</button>
+              </Link>}
+              <BookTable books={bookColor ? books.filter(book => book.color === bookColor) : books} />
+            </div>;
   }
 }
 
