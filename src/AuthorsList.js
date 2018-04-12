bb904880bb947e9424d76578f88a1f0f16a37cc0
Octowl <alsaff1987@gmail.com>
2018-04-12 14:59:37 +0300

    Fix indentation

--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -4,22 +4,22 @@ import {observer} from 'mobx-react';
 import AuthorCard from './AuthorCard';
 import SearchBar from './SearchBar';
 
-    function AuthorsList(props) {
+function AuthorsList(props) {
 
-      const authors = props.authorStore.filteredAuthors.map(author => (
-        <AuthorCard key={author.first_name + author.last_name}
-                    author={author}/>
-        ));
+  const authors = props.authorStore.filteredAuthors.map(author => (
+    <AuthorCard key={author.first_name + author.last_name}
+                author={author}/>
+    ));
 
-      return (
-        <div className="authors">
-          <h3>Authors</h3>
-          <SearchBar store={props.authorStore} />
-          <div className="row">
-            {authors}
-          </div>
-        </div>
-      );
-    }
+  return (
+    <div className="authors">
+      <h3>Authors</h3>
+      <SearchBar store={props.authorStore} />
+      <div className="row">
+        {authors}
+      </div>
+    </div>
+  );
+}
 
-    export default observer(AuthorsList);
+export default observer(AuthorsList);
